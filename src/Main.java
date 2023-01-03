aba91e02adf9f738358ec31208b5df372065e057
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-03 11:29:51 +0700

    Homework is ready

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-            
+    public static void main(String[] args)
+    {
 
+        int ticket_price = 7460;
+        int rub_for_a_miles = 20;
+        int bonus_miles = (ticket_price / rub_for_a_miles);
+
+        System.out.println("Начислено бонусных миль " + bonus_miles);
+
+    }
 }
